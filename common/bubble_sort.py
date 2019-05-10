3fa995b8d50d257ad4eeec9f3b9e9168c9c3796e
zhulei2017 <zhulei2017@outlook.com>
2019-05-10 10:15:16 +0800

    add bubble sort algorithm2

--- common/bubble_sort.py
+++ common/bubble_sort.py
@@ -5,7 +5,19 @@ def bubble_sort(lst):
                 lst[j-1], lst[j] = lst[j], lst[j-1]
 
 
+def bubble_sort_flag(lst):
+    for i in range(0, len(lst)):
+        found = False
+        for j in range(0, len(lst)-i-1):
+            if lst[j] > lst[j+1]:
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+                found = True
+        if not found:
+            break
+
+
 if __name__ == "__main__":
     ret = [2, 4, 5, 7, 2, 6]
-    bubble_sort(ret)
+    # bubble_sort(ret)
+    bubble_sort_flag(ret)
     print(ret)
