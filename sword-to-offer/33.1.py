40e721d286912324b1af5f43448c60bda638b8f9
zhulei2017 <zhulei2017@outlook.com>
2019-05-30 22:42:59 +0800

    add python offer 33

--- sword-to-offer/33.1.py
+++ sword-to-offer/33.1.py
@@ -1,14 +1,14 @@
-def cmp(a, b):
-    return int(str(a) + str(b)) - int(str(b) + str(a))
+from functools import cmp_to_key
 
 
-def print_mini(nums):
-    if not nums:
-        return []
-    res = int(''.join([str(num) for num in sorted(nums, cmp=cmp)]))
-    print(res)
+def printminnumber(numbers):
+    nums = list(map(str, numbers))    # map:将numbers的list的数字转换为字符串，list: 字符串列表['321', '32', '3']
+    # cmp_to_key  .sort(key=cmp_to_key(cmp)
+    # lambda 匿名函数：x,y为字符串，x>y:1, x=y:0, x<y:-1
+    nums.sort(key=cmp_to_key(lambda x, y: ((x+y) > (y+x)) - ((y+x) > (x+y))))
+    return ''.join(nums)
 
 
 if __name__ == '__main__':
-    test = [321, 32, 3]
-    print_mini(test)
+    test = [321, 232, 113]
+    print(printminnumber(test))
